1d93a2973517f5799c9a87d155f2b249f4423f31
Nick Lee <nickoless.lee@gmail.com>
2018-01-31 20:32:24 +0000

    Implemented createTweetElement function - pulls data from an object (currently hardcoded) and renders tweet.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,46 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": {
+      "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+      "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+      "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+    },
+    "handle": "@SirIsaac"
+  },
+  "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+  },
+  "created_at": 1461116232227
+};
+
+
+function createTweetElement (tweetData) {
+  let $tweet = $("<article>").addClass("tweet");
+  let $header = $("<header>")
+  let $img = $("<img>").attr("src", tweetData.user.avatars.regular);
+  let $h2 = $("<h2>").text(tweetData.user.name)
+  let $headerSpan = $("<span>").text(tweetData.user.handle);
+  let $p = $("<p>").text(tweetData.content.text);
+  let $footer = $("<footer>");
+  let $footerSpan = $("<span>").text(tweetData.created_at);
+
+  $tweet.append($header, $p, $footer);
+  $header.append($img, $h2 ,$headerSpan);
+  $footer.append($footerSpan);
+
+  return $tweet;
+}
+
+$(function()  {
+  $('#tweets-container').append(createTweetElement(tweetData));
+
+})
+
+
+
+
+// ($tweet);
