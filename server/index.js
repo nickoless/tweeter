472481ec2d710eaccd2db57a82f3acad5a0d9750
Nick Lee <nickoless.lee@gmail.com>
2018-02-02 22:19:39 +0000

    Implemented Sass and middleware. css files have been converted to sscss format and stored separately.

--- server/index.js
+++ server/index.js
@@ -3,22 +3,27 @@
 // Basic express setup:
 
 const PORT          = 8080;
-const nodeSassMiddleware = require('node-sass-middleware')
 const express       = require("express");
 const bodyParser    = require("body-parser");
 const app           = express();
 
+// Sass
+
+const nodeSassMiddleware = require('node-sass-middleware');
+const path = require('path');
+
 app.use(nodeSassMiddleware({
-  src: path.join(__dirname, "sass"),
-  dest: path.join(__dirname, "public"),
+  src: path.join(__dirname, "../sass"),
+  dest: path.join(__dirname, "../public/styles"),
   debug: true,
-  outputStyle: "compressed"
+  outputStyle: "compressed",
+  prefix: "/styles"
 }));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-// The in-memory database of tweets. It's a basic object with an array in it.
+// Mongo DB
 
 const MongoClient = require("mongodb").MongoClient;
 const MONGODB_URI = "mongodb://localhost:27017/tweeter";
@@ -35,19 +40,6 @@ MongoClient.connect(MONGODB_URI, (err, db) => {
 
 });
 
-// The `data-helpers` module provides an interface to the database of tweets.
-
-// This simple interface layer has a big benefit: we could switch out the
-// actual database it uses and see little to no changes elsewhere in the code
-// (hint hint).
-
-// Because it exports a function that expects the `db` as a parameter, we can
-// require it and pass the `db` parameter immediately:
-
-// The `tweets-routes` module works similarly: we pass it the `DataHelpers` object
-// so it can define routes that use it to interact with the data layer.
-
-// Mount the tweets routes at the "/tweets" path prefix:
 
 app.listen(PORT, () => {
   console.log("Tweeting to port " + PORT);
