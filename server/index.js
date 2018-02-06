25b2fdcb798094555c6742cc1383e9cfcbba33cc
Nick Lee <nickoless.lee@gmail.com>
2018-02-06 04:12:20 +0000

    switched default port back to 8080, testing connection issues with heroku'

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 // Basic express setup:
 
-const PORT          = process.env.PORT  || 8080;
+const PORT          = 8080;
 const express       = require("express");
 const bodyParser    = require("body-parser");
 const app           = express();
