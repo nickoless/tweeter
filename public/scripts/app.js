03d71ac7d4a0cffd4f5be3ce9747e58516eda1e2
Nick Lee <nickoless.lee@gmail.com>
2018-02-06 07:34:05 +0000

    Fixed issue where user could submit tweet full of whitespace

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,75 +1,23 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
 
 
 $(document).ready(function() {
-
-  /* Function to reference time since submit - **Rob from Stack Overflow**
-  (https://stackoverflow.com/questions/3177836/how-to-format-time-since-xxx-e-g-4-minutes-ago-similar-to-stack-exchange-site)
-  */
-
-  var timeSince = function(date) {
-    if (typeof date !== 'object') {
-      date = new Date(date);
-    }
   
-    var seconds = Math.floor((new Date() - date) / 1000);
-    var intervalType;
-  
-    var interval = Math.floor(seconds / 31536000);
-    if (interval >= 1) {
-      intervalType = 'year';
-    } else {
-      interval = Math.floor(seconds / 2592000);
-      if (interval >= 1) {
-        intervalType = 'month';
-      } else {
-        interval = Math.floor(seconds / 86400);
-        if (interval >= 1) {
-          intervalType = 'day';
-        } else {
-          interval = Math.floor(seconds / 3600);
-          if (interval >= 1) {
-            intervalType = "hour";
-          } else {
-            interval = Math.floor(seconds / 60);
-            if (interval >= 1) {
-              intervalType = "minute";
-            } else {
-              interval = seconds;
-              intervalType = "second";
-            }
-          }
-        }
-      }
-    }
-  
-    if (interval > 1 || interval === 0) {
-      intervalType += 's';
-    }
-  
-    return interval + ' ' + intervalType;
-  };
-
   // creates tweet based on HTML index, recreates DOM using Jquery --------
 
   function createTweetElement (tweetData) {
       
-    let $tweet = $("<article>").addClass("tweet");
-    let $header = $("<header>");
-    let $img = $("<img>").attr("src", tweetData.user.avatars.regular);
-    let $h2 = $("<h2>").text(tweetData.user.name);
-    let $headerSpan = $("<span>").text(tweetData.user.handle);
-    let $p = $("<p>").text(tweetData.content.text);
-    let $footer = $("<footer>");
-    let timeStamp = (timeSince(tweetData.created_at) + " ago");
-    let $footerSpan = $("<span>").text(timeStamp);
-    let $footerHeart = $("<i>").addClass("far fa-heart", "icon");
-    let $footerFlag =  $("<i>").addClass("far fa-flag", "icon");
-    let $footerRetweet = $("<i>").addClass("fas fa-retweet", "icon");
+    const $tweet = $("<article>").addClass("tweet");
+    const $header = $("<header>");
+    const $img = $("<img>").attr("src", tweetData.user.avatars.regular);
+    const $h2 = $("<h2>").text(tweetData.user.name);
+    const $headerSpan = $("<span>").text(tweetData.user.handle);
+    const $p = $("<p>").text(tweetData.content.text);
+    const $footer = $("<footer>");
+    const timeStamp = (tweetData.created_at);
+    const $footerSpan = $("<span>").text(timeStamp);
+    const $footerHeart = $("<i>").addClass("far fa-heart", "icon");
+    const $footerFlag =  $("<i>").addClass("far fa-flag", "icon");
+    const $footerRetweet = $("<i>").addClass("fas fa-retweet", "icon");
 
     $tweet.append($header, $p, $footer);
     $header.append($img, $h2, $headerSpan);
@@ -111,16 +59,16 @@ $(document).ready(function() {
 
   $(".new-tweet form").on("submit", function(event) {
     event.preventDefault();
-    let tweetLength = $("#textArea").val().length;
-    if (tweetLength === 0) {
+    let tweet = $("#textArea").val();
+    if (tweet.trim().length === 0) {
       alert("Please enter a tweet");
       console.log('test');
-    } else if (tweetLength > 140) {
+    } else if (tweet.length > 140) {
       alert("Your tweet is too long!");
     } else {
-      let formData = $(this).serialize();
+      const formData = $(this).serialize();
       $.post('/tweets', formData).done(function() {
-        tweetLength = 0;
+        tweet.length = 0;
         $("#textArea").val("");
         $("#tweets-container").empty();
         loadTweets();
